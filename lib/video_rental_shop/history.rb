b94e3102a49900ac7acc023dae96ed00d27f6a98
Chelong <chelong821204@gmail.com>
2018-05-13 23:30:25 +0800

    Add Rental Class Template

---
+++ lib/video_rental_shop/history.rb
@@ -0,0 +1,17 @@
+module VideoRentalShop
+  class Customer
+
+    def initialize(name)
+      @name = name
+      @rented_list = []
+    end
+
+    private
+
+    def create_rental
+    end
+
+    def rent_video
+    end
+  end
+end
---
+++ lib/video_rental_shop/rental.rb
@@ -0,0 +1,14 @@
+module VideoRentalShop
+  class Rental
+
+    def initialize
+      @video_list = []
+      @rent_date = Time.now
+      @due_days = 0
+    end
+
+    def is_dued?
+
+    end
+  end
+end
