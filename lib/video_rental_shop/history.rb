3df96696f0cc840e558032f0a0ceb66e12883d3f
Chelong <chelong821204@gmail.com>
2018-05-13 23:41:00 +0800

    Add History Class Template

--- lib/video_rental_shop/history.rb
+++ lib/video_rental_shop/history.rb
@@ -1,17 +1,16 @@
 module VideoRentalShop
-  class Customer
+  class History
 
-    def initialize(name)
-      @name = name
-      @rented_list = []
+    def initialize(name, video_list, rent_date, due_days, price_amount)
+      @customer = name
+      @video_list = video_list
+      @rent_date = rent_date
+      @due_days = due_days
+      @price_amount = price_amount
     end
 
-    private
-
-    def create_rental
-    end
-
-    def rent_video
+    def show
+      p "Histoy: #{ @customer } rentaled video_list form #{ @rent_date } for #{ @due_days }, and total price is #{ @price_amount }"
     end
   end
 end
